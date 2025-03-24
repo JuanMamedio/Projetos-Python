c028b51155bc75d1927f6470482e66aa7e9e6128
Juan Mamedio <140915478+JuanMamedio@users.noreply.github.com>
2025-03-23 22:28:28 -0300

    Desafio 4 - (Guanabara) V1.2
    Foi criado um sistema simples, para rodar no terminal, com a finalidade de, ler a mensagem que o usuario vai digitar, e em seguida, retornar as propriedade do que o usuario digitou, como exemplo, É string? É tudo escrito em maisculo, ou minusculo? é Alpha ou Alpha númerico? 
    
    obs: V1.2 (Adicionado novos parametros)

--- Desafio4.py
+++ Desafio4.py
@@ -1,15 +1,18 @@
 #O Usuário vai digitar algo, e vamos responder em que tipo se classifica oque ele digitou.
-
 Mensagem_Usuario = str(input('Digite alguma coisa!'))
-
-
 #É Alpha?
 Mensagem_Alpha = Mensagem_Usuario.isalpha()
 # É Alpha Num? 
 Mensagem_Alpha_Numerico = Mensagem_Usuario.isalnum()
 # Qual o tipo?
 Tipo_Mensagem = type(Mensagem_Usuario)
+# É maiuscula?
+Mensagem_Maiusculo = Mensagem_Usuario.isupper()
+#É minuscula?
+Mensagem_Minusculo = Mensagem_Usuario.islower()
 
 print(f"A Mensagem é Alpha? {Mensagem_Alpha} ")
 print(f"A Mensagem é Alpha Numerica? {Mensagem_Alpha_Numerico} ")
 print(f"Qual o tipo da Mensagem? {Tipo_Mensagem} ")
+print(f'A mensagem é tudo em Maiuscula? {Mensagem_Maiusculo}')
+print(f'A mensagem é tudo em minusculo? {Mensagem_Minusculo}')
